9520098cb114e317830901c7e381bbb3a16d7813
Jorge Garcia Ojeda <jor4300@gmail.com>
2025-05-06 18:14:21 -0300

    se acomodaron los componentes dentro de carpetas para mejor legibilidad

--- ecommerce-frontend-metodologia/src/components/screens/CatalogoScreen/CatalogoScreen.tsx
+++ ecommerce-frontend-metodologia/src/components/screens/CatalogoScreen/CatalogoScreen.tsx
@@ -1,8 +1,8 @@
-import { ColumnFilterCatalogo } from "../../ui/ColumnFilterCatalogo/ColumnFilterCatalogo";
+import { ColumnFilterCatalogo } from "../../ui/Catalogo/ColumnFilterCatalogo/ColumnFilterCatalogo";
 import { Footer } from "../../ui/Footer/Footer";
 import { HeaderDescuento } from "../../ui/HeaderDescuento/HeaderDescuento";
-import { NavBarCatalogo } from "../../ui/NavBarCatalogo/NavBarCatalogo";
-import { SectionImagenesCatalogo } from "../../ui/SectionImagenesCatalogo/SectionImagenesCatalogo";
+import { NavBarCatalogo } from "../../ui/Catalogo/NavBarCatalogo/NavBarCatalogo";
+import { SectionImagenesCatalogo } from "../../ui/Catalogo/SectionImagenesCatalogo/SectionImagenesCatalogo";
 import styles from "./CatalogoScreen.module.css";
 
 export const CatalogoScreen = () => {
--- ecommerce-frontend-metodologia/src/components/screens/landingScreen/LandingScreen.tsx
+++ ecommerce-frontend-metodologia/src/components/screens/landingScreen/LandingScreen.tsx
@@ -1,9 +1,9 @@
 
-import CarouselComponent from '../../ui/Carrusel/CarouselComponent'
+import CarouselComponent from '../../ui/Landing/Carrusel/CarouselComponent'
 import { Footer } from '../../ui/Footer/Footer'
 import { HeaderDescuento } from '../../ui/HeaderDescuento/HeaderDescuento'
-import { NavBarLanding } from '../../ui/NavBarLanding/NavBarLanding'
-import { PrincipalImagesLanding } from '../../ui/PrincipalImagesLanding/PrincipalImagesLanding'
+import { NavBarLanding } from '../../ui/Landing/NavBarLanding/NavBarLanding'
+import { PrincipalImagesLanding } from '../../ui/Landing/PrincipalImagesLanding/PrincipalImagesLanding'
 import styles from './LandingScreen.module.css'
 
 export const LandingScreen = () => {
